c3cf3d88d310f115b96e8891a5b44f3c11e11e3c
ParthaPradeepNath <parthanath96134@gmail.com>
2026-02-22 23:14:44 +0530

    implement better-auth

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import subjectsRouter from "./routes/subjects";
 import cors from "cors";
 import securiyMiddleware from "./middleware/security";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app = express();
 const PORT = 8000;
@@ -15,12 +17,14 @@ app.use(
     origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  })
+  }),
 );
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
+
 app.use(express.json());
 
-app.use(securiyMiddleware)
-
+app.use(securiyMiddleware);
 
 app.use("/api/subjects", subjectsRouter);
 
